9a5256bfd237749b7ddf960b4535b5f3715d7d6e
osvaldecir viana <manocatanduva@gmail.com>
2024-07-25 15:32:55 -0300

    //

--- index.js
+++ index.js
@@ -7,18 +7,36 @@ let tasks = []
 form.addEventListener('submit', (event) => {
     event.preventDefault() //evita o comportamento padrao//
     const taskTitle = taskTitleInput.value
-    
+
     if (taskTitle.length < 3) {
         alert('sua tarefa precisa ao menos de 3 caracteres')
         return
     }
-    tasks.push(taskTitle)
+    tasks.push({
+        title: taskTitle,
+        done: false,
+    )}
     //adicionando  a nova tarefa no html//
     const li = document.createElement('li')
-    li.textContent = taskTitle
+
+    const input = document.createElement('input')
+
+    input.setAttribute('type','checkbox')
+
+    const span = document.createElement('span')
+    span.textContent = taskTitle
+
+    const button = document.createElement('button')
+
+    button.textContent = 'remover'
+
+    li.appendChild(input)
+    li.appendChild(span)
+    li.appendChild(button)    
+
     todoListUL.appendChild(li)
+
     taskTitleInput.value = ''
 
 
 
-})
